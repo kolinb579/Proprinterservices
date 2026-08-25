1109c6e12729d76b217a77578f08b410bff049d5
kolinb579 <kolinb579@gmail.com>
2026-08-25 22:26:23 +0530

    Add web analytics script to index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<script src="https://app.zerocloak.com/web-analytics.js/l4sv6idby1"></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>ProPrinterServices | Onsite Printer Services</title>
